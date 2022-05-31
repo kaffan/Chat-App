2dcb44d933679a08ac199e6d0eae2f61ec50a4b2
Khan Affan Sibgatullah <53649298+kaffan@users.noreply.github.com>
2022-05-31 17:13:05 +0530

    Update HeadPart.jsx

--- src/Components/Rightpane/HeadPart.jsx
+++ src/Components/Rightpane/HeadPart.jsx
@@ -12,9 +12,9 @@ const HeadPart = (props) =>
     function stringAvatar(name) {
         name = name.replace(/^\s+|\s+$/gm,'');
         const firstName = name.split(' ')[0][0];
-        const lastName = name.split(' ')[1][0];
+        const lastName = name.split(' ')[1];
         return {
-          children: `${firstName}${(lastName) ? lastName : ''}`,
+          children: `${firstName}${(lastName) ? lastName[0] : ''}`,
         };
       }
     const isChat = useSelector(state=>state.isChat);
